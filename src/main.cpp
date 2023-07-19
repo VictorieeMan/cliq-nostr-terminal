19006712d768df9f2f7ca18dcf04bb5305c1389f
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2023-07-19 11:38:18 +0200

    added comment to includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-#include "libwebsockets.h"
-#include "simdjson.h"
+// External libraries
+#include <simdjson.h>
+#include <libwebsockets.h>
 
 int main(int argc, char* argv[]) {
 	std::cout << "Hello libwebsockets and simdjson!" << std::endl;
